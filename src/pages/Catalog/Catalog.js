5fc5631646a7c205da1f32eaf264ac3ee2950ba2
Nhi Dinh <tuyetnhi1607@github.com>
2021-11-05 19:32:33 +0700

    detail movie 2

--- src/pages/Catalog/Catalog.js
+++ src/pages/Catalog/Catalog.js
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react'
+import {useParams} from 'react-router'
+import tmdbApi from '../../api/tmdbApi';
 
 function Catalog() {
+    const {category} = useParams(); 
+    const [list, setList] = useState([]);
+    const params = {page: 1};
+
+    useEffect(() => {
+        const getMovies = async () => {
+            if(category === "movie"){
+                await tmdbApi.getMoviesList(category, {params})
+                .then(response=>{
+                    setList(response.results);
+                })
+                .catch(error => {console.error(error)
+                })
+            }else {
+                await tmdbApi.getTvList(category, {params})
+                .then(response=>{
+                    setList(response.results);
+                })
+                .catch(error => {console.error(error);})
+            }
+            
+        }
+        getMovies();
+    },[])
+
+   console.log("list: ", list);
     return (
         <div>
             
